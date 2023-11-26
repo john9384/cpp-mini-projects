b4793336b2821cbf5732b3896bb87130e5ba9f1a
John Damilola Ogungbure <jogungbure@gmail.com>
2023-11-26 21:36:05 +0100

    Calculator

---
+++ cgpa-calculatore.cpp
@@ -0,0 +1,8 @@
+//
+//  cgpa-calculatore.cpp
+//  CGPA-calculatore
+//
+//  Created by Damilola on 24/11/2023.
+//
+
+#include <stdio.h>
---
+++ rock-paper-scissors.cpp
@@ -0,0 +1,8 @@
+//
+//  rock-paper-scissors.cpp
+//  CGPA-calculatore
+//
+//  Created by Damilola on 24/11/2023.
+//
+
+#include "rock-paper-scissors.hpp"
