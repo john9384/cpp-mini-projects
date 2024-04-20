ef363e3ce037b845111b7b14fa8a31178fb19e35
John Damilola Ogungbure <jogungbure@gmail.com>
2024-04-20 08:04:30 +0100

    ds list

---
+++ cpp_mini_projects/cpp_mini_projects/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cpp_mini_projects
+//
+//  Created by Damilola on 04/01/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
